58688c283634deb5d7440a6c30df622054eac580
xxputri <putriintanamaliadahlan@gmail.com>
2022-06-08 13:25:29 +0700

    keempat

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 setInterval(function () {
-    fetch("http://smart-glass-7.herokuapp.com/api")
+    fetch("https://smart-glass-7.herokuapp.com/api")
       .then((hasil) => hasil.json())
       .then((res) => {
         console.log(res);
-        document.getElementById("cahaya").innerHTML = res.ldr;
+        document.getElementById("ldr").innerHTML = res.ldr;
+        document.getElementById("cahaya").innerHTML = res.cahaya;
+        document.getElementById("kaca").innerHTML = res.kaca;
       });
   }, 1000);
